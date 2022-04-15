69b9ff3d76f254d08bfe3d50e6b2c320c4315f8e
Sean Cabalse <sean.cabalse@globe.com.ph>
2022-04-15 14:13:02 +0800

    Added images, icons and assets for i_am_rich

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[600],
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+            // image: NetworkImage(
+            //     'https://toppng.com/uploads/preview/ruby-gem-11552769604dg0io451pz.png'),
+          ),
+        ),
       ),
     ),
   );
